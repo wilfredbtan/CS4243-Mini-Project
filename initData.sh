ab4bccadc0afce0baf7685e17cd97b4fd5cdac0d
Wilfred Bradley Tan <wilfredbtan@gmail.com>
2022-11-08 20:09:04 +0800

    Update script to run on colab

--- initData.sh
+++ initData.sh
@@ -1,9 +1,12 @@
+echo "File path: $1"
+
 typeset NORMAL_TEMP=$( mktemp )
 touch "${NORMAL_TEMP}"
-typeset NORMAL_TEMP_LABELLED=$( mktemp )
+typeset NORMAL_TEMP_LABELLED=$(mktemp)
 touch "${NORMAL_TEMP_LABELLED}"
 
-ls cs4243_smallest/normal > "${NORMAL_TEMP}"
+ls "$1/normal" > "${NORMAL_TEMP}"
+echo "$1/normal"
 awk '{print "normal/"$0",0"}' "${NORMAL_TEMP}" > "${NORMAL_TEMP_LABELLED}"
 
 typeset CARRYING_TEMP=$( mktemp )
@@ -11,7 +14,8 @@ touch "${CARRYING_TEMP}"
 typeset CARRYING_TEMP_LABELLED=$( mktemp )
 touch "${CARRYING_TEMP_LABELLED}"
 
-ls cs4243_smallest/carrying > "${CARRYING_TEMP}"
+ls "$1/carrying" > "${CARRYING_TEMP}"
+echo "$1/carrying"
 awk '{print "carrying/"$0",1"}' "${CARRYING_TEMP}" > "${CARRYING_TEMP_LABELLED}"
 
 typeset THREAT_TEMP=$( mktemp )
@@ -19,7 +23,8 @@ touch "${THREAT_TEMP}"
 typeset THREAT_TEMP_LABELLED=$( mktemp )
 touch "${THREAT_TEMP_LABELLED}"
 
-ls cs4243_smallest/threat > "${THREAT_TEMP}"
+ls "$1/threat" > "${THREAT_TEMP}"
+echo "$1/threat"
 awk '{print "threat/"$0",2"}' "${THREAT_TEMP}" > "${THREAT_TEMP_LABELLED}"
 
 echo "directory,label" > dataset.csv
